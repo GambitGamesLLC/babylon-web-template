556bc80546ac05a7de7d4836422b82ab7cbc971e
Derrick Barra <gambitgamesllc@gmail.com>
2022-09-26 19:29:24 -0400

    Added @babylonjs/core as an external dependency

--- test.ts
+++ test.ts
@@ -2,6 +2,28 @@
 //@ts-ignore
 import * as module from "./lib/main.ts";
 
+//Import the Babylonjs Core package
+import * as BABYLON from "@babylonjs/core";
+
+//Add Babylon namespace to the global window
+(window as any).BABYLON = {...BABYLON};
+
+//Show the Babylonjs inspector, requires that the BABYLON namespace has been added to the Window object
+/*
+//Requires you already have a Babylonjs scene created
+scene.debugLayer.show({
+    embedMode: true,
+    enableClose: true,
+    enablePopup: false,
+    //@ts-ignore
+    globalRoot: document.getElementById("app"),
+    handleResize: true,
+    overlay: true,
+    showExplorer: true,
+    showInspector: true
+});
+*/
+
 async function Run()
 {
     try 
